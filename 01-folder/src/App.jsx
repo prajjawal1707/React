855aecbb58f09b9ee7ac0de60968aae8bb557ab9
prajjawal1707 <147046715+prajjawal1707@users.noreply.github.com>
2026-09-02 20:22:56 +0530

    adding folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+   <>
+   <div className="App">
+      <h1>Hello, React!</h1>
+      <p>Welcome to my React application.</p>
+    </div>
+    <div>
+      <h2>what i am wrting</h2>
+    </div>
+   </>
+  );
+}
+
+export default App;
