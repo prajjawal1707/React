e65d28a1cc25a92e682f695d3fa8760a65692967
prajjawal1707 <147046715+prajjawal1707@users.noreply.github.com>
2026-09-03 00:12:51 +0530

    adding the react frontend project

---
+++ 07-project/src/App.jsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      asdfghjk
+    </div>
+  );
+}
+
+export default App;
---
+++ 07-project/src/components/section01/Section01.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Section01 = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Section01;
